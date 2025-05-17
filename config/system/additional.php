34b9d2e1941185443e4226b93f0517ab0618833f
Jochen <jochen.roth@b13.com>
2025-05-17 09:55:12 +0000

    [TASK] Add mailpit

--- config/system/additional.php
+++ config/system/additional.php
@@ -20,8 +20,12 @@ $GLOBALS['TYPO3_CONF_VARS'] = array_replace_recursive($GLOBALS['TYPO3_CONF_VARS'
             ],
         ],
     ],
+    // This mail configuration sends all emails to mailpit
     'MAIL' => [
-        // @todo: Enable mailpit
+        'transport' => 'smtp',
+        'transport_smtp_encrypt' => false,
+        'transport_smtp_server' => 'mail:1025',
+        'defaultMailFromAddress' => 'info@example.com'
     ],
     'SYS' => [
         'devIPmask' => '',
